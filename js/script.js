0786d474770ba31b5428243c70754dd85b292c5b
shubham-paul <shubhampaul@gmail.com>
2014-12-28 23:10:59 +0530

    done

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+
+var MainController = function($scope, $http){
+	
+	var onUserComplete = function(response) {
+		
+		$scope.user = response.data;
+	}
+	
+	$scope.message = "Hello Shubham!";
+	$http.get("https://api.github.com/users/robconery").then(onUserComplete);
+	
+	
+	
+	
+};
